2d5c2ab9745d0ec94fb827948227e16c27d7312b
Kaustubh Shejole <sshejole132@gmail.com>
2026-08-16 11:15:16 +0530

    Updated UAI 2026

--- scripts/main.js
+++ scripts/main.js
@@ -24,6 +24,7 @@ ${authorsHTML}.<br>
 ${pub.githubLink ? `<a target="_blank" href="${pub.githubLink}">[GitHub]</a>` : ""}
 ${pub.videoLink ? `<a target="_blank" href="${pub.videoLink}">[Video]</a>` : ""}
 ${pub.posterLink ? `<a target="_blank" href="${pub.posterLink}">[Poster]</a>` : ""}
+${pub.arxivLink ? `<a target="_blank" href="${pub.arxivLink}">[Arxiv]</a>` : ""}
 ${pub.showAbstract ? `<button class="abstract-btn" data-target="${abstractId}">[Abstract]</button>` : ""}
 <div id="${abstractId}" class="abstract">${pub.abstract}</div>
 `;
